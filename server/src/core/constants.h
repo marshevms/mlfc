3ef3adafcdc44d0817f6cb80fb46d92b43ea375f
Marshev M.S <marshev_ms@protonmail.com>
2020-07-01 00:54:35 +0400

    Add addresses for fan mode and cooler boost, enum classes of states for fan mode and cooler boost

--- server/src/core/constants.h
+++ server/src/core/constants.h
@@ -15,7 +15,9 @@ static constexpr char kECFilePath[]{"/sys/kernel/debug/ec/ec0/io"};
 static constexpr char kECFilePath[]{"io"};
 #endif
 
-static constexpr int kFanRPM = 478000;
+static constexpr int kFanRPM{478000};
+static constexpr int kFanMode{0xF4};
+static constexpr int kCoolerBoost{0x98};
 
 struct cpu
 {
@@ -28,6 +30,29 @@ struct gpu
     static constexpr int kRealtimeTemp{0x80};
     static constexpr std::array<int, 2> kRealtimeFanRPM{0xCA, 0xCB};
 };
+
+constexpr int kCoolerBoostON{0x80};
+constexpr int kCoolerBoostOFF{0x00};
+
+enum class CoolerBoost
+{
+    ONN,
+    OFF,
+    Unknown
+};
+
+constexpr int kFanModeAuto{0x0C};
+constexpr int kFanModeBasic{0x4C};
+constexpr int kFanModeAdvanced{0x8C};
+
+enum class FanMode
+{
+    Auto,
+    Basic,
+    Advanced,
+    Unknown
+};
+
 
 } // namespace core
 
