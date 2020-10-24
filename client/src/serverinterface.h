5c0231eb515aaa6c98ff8fc191919697832c4336
Marshev M.S <msmarshev@gmail.com>
2020-10-24 23:43:50 +0400

    change DBus function code style because of server

--- client/src/serverinterface.h
+++ client/src/serverinterface.h
@@ -26,80 +26,80 @@ public Q_SLOTS: // METHODS
     inline QDBusPendingReply<EnumerationStorage::CoolerBoost> coolerBoost()
     {
         QList<QVariant> argumentList;
-        return asyncCallWithArgumentList(QStringLiteral("CoolerBoost"), argumentList);
+        return asyncCallWithArgumentList(QStringLiteral("coolerBoost"), argumentList);
     }
 
     inline QDBusPendingReply<EnumerationStorage::FanMode> fanMode()
     {
         QList<QVariant> argumentList;
-        return asyncCallWithArgumentList(QStringLiteral("FanMode"), argumentList);
+        return asyncCallWithArgumentList(QStringLiteral("fanMode"), argumentList);
     }
 
     inline QDBusPendingReply<bool> setCoolerBoost(EnumerationStorage::CoolerBoost cooler_boost)
     {
         QList<QVariant> argumentList;
         argumentList << QVariant::fromValue(cooler_boost);
-        return asyncCallWithArgumentList(QStringLiteral("SetCoolerBoost"), argumentList);
+        return asyncCallWithArgumentList(QStringLiteral("setCoolerBoost"), argumentList);
     }
 
     inline QDBusPendingReply<bool> setFanMode(EnumerationStorage::FanMode fan_mode)
     {
         QList<QVariant> argumentList;
         argumentList << QVariant::fromValue(fan_mode);
-        return asyncCallWithArgumentList(QStringLiteral("SetFanMode"), argumentList);
+        return asyncCallWithArgumentList(QStringLiteral("setFanMode"), argumentList);
     }
 
     inline QDBusPendingReply<bool> setUpdateInterval(int interval)
     {
         QList<QVariant> argumentList;
         argumentList << QVariant::fromValue(interval);
-        return asyncCallWithArgumentList(QStringLiteral("SetUpdateInterval"), argumentList);
+        return asyncCallWithArgumentList(QStringLiteral("setUpdateInterval"), argumentList);
     }
 
     inline QDBusPendingReply<QVector<int>> cpuTemps()
     {
-        return asyncCallWithArgumentList(QStringLiteral("CPUTemps"), {});
+        return asyncCallWithArgumentList(QStringLiteral("cpuTemps"), {});
     }
 
     inline QDBusPendingReply<QVector<int>> cpuFanSpeeds()
     {
-        return asyncCallWithArgumentList(QStringLiteral("CPUFanSpeeds"), {});
+        return asyncCallWithArgumentList(QStringLiteral("cpuFanSpeeds"), {});
     }
 
     inline QDBusPendingReply<QVector<int>> gpuTemps()
     {
-        return asyncCallWithArgumentList(QStringLiteral("GPUTemps"), {});
+        return asyncCallWithArgumentList(QStringLiteral("gpuTemps"), {});
     }
 
     inline QDBusPendingReply<QVector<int>> gpuFanSpeeds()
     {
-        return asyncCallWithArgumentList(QStringLiteral("GPUFanSpeeds"), {});
+        return asyncCallWithArgumentList(QStringLiteral("gpuFanSpeeds"), {});
     }
 
     inline QDBusPendingReply<bool> start()
     {
         QList<QVariant> argumentList;
-        return asyncCallWithArgumentList(QStringLiteral("Start"), argumentList);
+        return asyncCallWithArgumentList(QStringLiteral("start"), argumentList);
     }
 
     inline QDBusPendingReply<int> updateInterval()
     {
         QList<QVariant> argumentList;
-        return asyncCallWithArgumentList(QStringLiteral("UpdateInterval"), argumentList);
+        return asyncCallWithArgumentList(QStringLiteral("updateInterval"), argumentList);
     }
 
     inline QDBusPendingReply<QString> lastError()
     {
         QList<QVariant> argumentList;
-        return asyncCallWithArgumentList(QStringLiteral("last_error"), argumentList);
+        return asyncCallWithArgumentList(QStringLiteral("lastError"), argumentList);
     }
 
 Q_SIGNALS: // SIGNALS
-    void AnErrorOccured(const QString &error);
-    void RealtimeCPUFanRPM(int rpm);
-    void RealtimeCPUTemp(int temp);
-    void RealtimeGPUFanRPM(int rpm);
-    void RealtimeGPUTemp(int temp);
+    void anErrorOccured(const QString &error);
+    void realtimeCPUFanRPM(int rpm);
+    void realtimeCPUTemp(int temp);
+    void realtimeGPUFanRPM(int rpm);
+    void realtimeGPUTemp(int temp);
 };
 
 }
